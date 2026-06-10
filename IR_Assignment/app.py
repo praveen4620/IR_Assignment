caecc1189e912198eed2f2eae480602d7003f7be
PanaPana PraveenKumar <2025aa05841@wilp.bits-pilani.ac.in>
2026-06-10 14:22:36 +0530

    Updated app

--- IR_Assignment/app.py
+++ IR_Assignment/app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1rJp_uUWON3DmC7GLb3ij83oebscZiljq
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import re
